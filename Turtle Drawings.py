4b7a70580bacb6c119e7e72d1cdeeee80804aa35
emeroconnor333 <117350531+emeroconnor333@users.noreply.github.com>
2023-09-24 22:12:47 +0100

    Update Turtle Drawings.py

--- Turtle Drawings.py
+++ Turtle Drawings.py
@@ -4,8 +4,8 @@ import turtle
 #set-up
 turtle.bgcolor("black") #background color
 turtle.pensize(2)
-turtle.color("pink")
-turtle.speed(10.5)
+turtle.color("white")
+turtle.speed(3)
 
 #square
 #turtle.forward(50)
@@ -26,9 +26,28 @@ turtle.speed(10.5)
 #turtle.done()
 
 #circles pattern 2
-for i in range(12):
- for colors in ["red", "orange", "yellow", "green", "blue", "purple"]:
-  turtle.color(colors)
-  turtle.circle(150)
-  turtle.left(5)
+# for i in range(12):
+#  for colors in ["red", "orange", "yellow", "green", "blue", "purple"]:
+#   turtle.color(colors)
+#   turtle.circle(150)
+#   turtle.left(5)
+# turtle.done()
+
+#spider web
+for i in range(10, 200, 30):
+    turtle.circle(i)
+    turtle.pu()
+    turtle.right(90)
+    turtle.fd(30)
+    turtle.left(90)
+    turtle.pd()
+
+for i in range(15):
+    turtle.pu()
+    turtle.goto(0, 10)
+    turtle.left(24)
+    turtle.pd()
+    turtle.fd(250)
+
+
 turtle.done()
